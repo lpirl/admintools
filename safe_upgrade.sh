069a6492114f8ba7efc85d26f0ec23af548a9653
Lukas Pirl <git@lukas-pirl.de>
2014-01-10 10:51:10 +0100

    add script to perform an aptitude safe upgrade

---
+++ safe_upgrade.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#
+# Script waits for the machine to reach the Internet and performs an
+# aptitude update and safe-upgrade in a screen session in case the is
+# user interaction required
+#
+
+renice 15 $$
+ionice -c3 -p $$
+
+while true; do ping -c1 8.8.8.8 > /dev/null && break; done
+
+CMD=""
+
+for O in update safe-upgrade autoclean
+do
+	CMD+="/usr/bin/aptitude -y $O"
+done
+
+screen -dmS upgrade bash -xc "$CMD"
