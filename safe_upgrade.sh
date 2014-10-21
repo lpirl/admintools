851b913e40a957af1566eb0904ca8e1d9a0e090b
Lukas Pirl <git@lukas-pirl.de>
2014-10-21 10:05:10 +0200

    added dependency checks and a lock file

--- safe_upgrade.sh
+++ safe_upgrade.sh
@@ -8,9 +8,32 @@
 # (I'd not advice to cron this on mission critical systems)
 #
 
+# lower priority
 (renice 15 $$ || true) > /dev/null
 (ionice -c3 -p $$ || true) > /dev/null
 
+# make sure only one instance runs at a time
+PIDFILE=/var/lock/$(basename $0)_$(whoami).lock
+if [ -e $PIDFILE ]; then
+	PID=`cat $PIDFILE`
+	if kill -0 &>1 > /dev/null $PID; then
+		echo "Already running"
+		exit 1
+	else
+		echo "deleting stale pidfile…"
+		rm $PIDFILE
+	fi
+fi
+trap "rm -f ${PIDFILE}; exit" INT TERM EXIT
+echo $$ > $PIDFILE
+
+# check dependencies
+type ping > /dev/null || exit 1
+type aptitude > /dev/null || exit 1
+type screen > /dev/null || exit 1
+type bash > /dev/null || exit 1
+
+# wait until online
 while true; do ping -c1 8.8.8.8 > /dev/null && break; done
 
 CMD=""
