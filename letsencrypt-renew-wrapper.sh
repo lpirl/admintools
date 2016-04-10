c04c126bf0002767eb7a8d3359b89662dc843109
Lukas Pirl <git@lukas-pirl.de>
2016-04-10 15:13:21 +0200

    added wrapper around `letsencrypt renew` to stop/start Web servers

---
+++ letsencrypt-renew-wrapper.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+for p in apache2 nginx; do type $p > /dev/null 2>&1 && service $p stop; done
+
+timeout -s KILL 30m letsencrypt renew > /dev/null
+exit_code=$?
+
+for p in apache2 nginx; do type $p > /dev/null 2>&1 && service $p start; done
+
+exit $exit_code
