3aef7f989a7c2db63fa8d6baff9ad24e45169447
Lukas Pirl <git@lukas-pirl.de>
2017-01-15 15:24:49 +0100

    properly wrapped main code in a main()

--- cli_application_template.py
+++ cli_application_template.py
@@ -9,37 +9,37 @@ import argparse
 from logging import getLogger, DEBUG, INFO, debug, info
 from subprocess import check_output
 
-if __name__ != '__main__':
-  raise NotImplementedError(
-    "Sorry, there is nothing to include - this is a CLI tool."
+def main():
+
+  parser = argparse.ArgumentParser(
+    description=__doc__,
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter
   )
 
-parser = argparse.ArgumentParser(
-  description=__doc__,
-  formatter_class=argparse.ArgumentDefaultsHelpFormatter
-)
+  parser.add_argument('-d', '--debug', action='store_true', default=False,
+                      help='turn on debug messages')
+  parser.add_argument('-v', '--verbose', action='store_true', default=False,
+                      help='turn on verbose messages')
+  # argument taking a list:
+  #parser.add_argument('-i', '--ignore', nargs='*', default=[],
+  #                    help='list of settings modules to ignore')
+  # argument taking a list:
+  #parser.add_argument('on_or_off', choices = ['on', 'off'], nargs='?',
+  #                    help='activate or deactivate')
 
-parser.add_argument('-d', '--debug', action='store_true', default=False,
-                    help='turn on debug messages')
-parser.add_argument('-v', '--verbose', action='store_true', default=False,
-                    help='turn on verbose messages')
-# argument taking a list:
-#parser.add_argument('-i', '--ignore', nargs='*', default=[],
-#                    help='list of settings modules to ignore')
-# argument taking a list:
-#parser.add_argument('on_or_off', choices = ['on', 'off'], nargs='?',
-#                    help='activate or deactivate')
+  args = parser.parse_args()
 
-args = parser.parse_args()
-
-# set up logger
-logger = getLogger()
-logger.name = ""
-if args.debug:
-  logger.setLevel(DEBUG)
-if args.verbose:
-  logger.setLevel(INFO)
+  # set up logger
+  logger = getLogger()
+  logger.name = ""
+  if args.debug:
+    logger.setLevel(DEBUG)
+  if args.verbose:
+    logger.setLevel(INFO)
 
 
-output = check_output(("echo", "Hello World"), universal_newlines=True)
-print(output.strip())
+  output = check_output(("echo", "Hello World"), universal_newlines=True)
+  print(output.strip())
+
+if __name__ != '__main__':
+  main()
