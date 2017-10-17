96bf6a8d681d7e27b949df46443e53af9457a1e5
Lukas Pirl <git@lukas-pirl.de>
2017-10-17 15:52:53 +0200

    added Cleaner to CLI application template

--- cli_application_template.py
+++ cli_application_template.py
@@ -2,14 +2,57 @@
 
 """
 This is a template for simple Python 3 CLI applications.
-(Replace this with your description)
+Replace this with your description.
 """
 
 import argparse
 from logging import getLogger, DEBUG, INFO, debug, info
 from subprocess import check_output
 
+
+
+class Cleaner(object):
+
+  def __init__(self):
+    self._jobs = []
+
+  def add_job(self, func, *args, **kwargs):
+    self._jobs.append((func, args, kwargs))
+
+  def do_all_jobs(self):
+    while self._jobs:
+      self.do_one_job()
+
+  def do_one_job(self):
+    # in reverse order:
+    func, args, kwargs = self._jobs.pop()
+    debug("cleanup: func=%s.%s, args=%r, kwargs=%r", func.__module__,
+         func.__name__, args, kwargs)
+    func(*args, **kwargs)
+
+
+
 def main():
+  cleaner = Cleaner()
+  abnormal_termination = False
+  try:
+    caught_main(cleaner)
+  except Exception as exception:
+    error("abnormal termination (see error at end of output)")
+    abnormal_termination = True
+    raise exception
+  finally:
+    debug("running cleanup jobs")
+    cleaner.do_all_jobs()
+
+  if abnormal_termination:
+    exit(1)
+  else:
+    debug("success - bye")
+
+
+
+def caught_main(cleaner):
 
   parser = argparse.ArgumentParser(
     description=__doc__,
@@ -24,7 +67,7 @@ def main():
   #parser.add_argument('-i', '--ignore', nargs='*', default=[],
   #                    help='list of settings modules to ignore')
   # argument taking a list:
-  #parser.add_argument('on_or_off', choices = ['on', 'off'], nargs='?',
+  #parser.add_argument('on_or_off', choices=('on', 'off'), nargs='?',
   #                    help='activate or deactivate')
 
   args = parser.parse_args()
@@ -41,5 +84,5 @@ def main():
   output = check_output(("echo", "Hello World"), universal_newlines=True)
   print(output.strip())
 
-if __name__ != '__main__':
+if __name__ == '__main__':
   main()
